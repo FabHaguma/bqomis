2db357e09cb68326f0afc127ec4b8c9621f95c04
fabrice137 <fabruz3@gmail.com>
2025-09-10 07:34:39 +0200

    book appointment fix - sept 10th

--- bqomis-backend/src/main/resources/db/migration/V1__init.sql
+++ bqomis-backend/src/main/resources/db/migration/V1__init.sql
@@ -3,7 +3,7 @@
 
 -- Create table for users
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     username VARCHAR(255),
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255),
@@ -14,21 +14,21 @@ CREATE TABLE IF NOT EXISTS users (
 
 -- Create table for roles
 CREATE TABLE IF NOT EXISTS roles (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE,
     description TEXT
 );
 
 -- Create table for districts
 CREATE TABLE IF NOT EXISTS districts (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     province VARCHAR(255) NOT NULL
 );
 
 -- Create table for branches
 CREATE TABLE IF NOT EXISTS branches (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     address TEXT,
     district VARCHAR(255) NOT NULL,
@@ -37,14 +37,14 @@ CREATE TABLE IF NOT EXISTS branches (
 
 -- Create table for services
 CREATE TABLE IF NOT EXISTS services (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     description TEXT
 );
 
 -- Create table for branch_services
 CREATE TABLE IF NOT EXISTS branch_services (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     branch_id INT NOT NULL REFERENCES branches(id) ON DELETE CASCADE,
     service_id INT NOT NULL REFERENCES services(id) ON DELETE CASCADE,
     available BOOLEAN NOT NULL DEFAULT TRUE -- Indicates if the service is available at the branch
@@ -52,7 +52,7 @@ CREATE TABLE IF NOT EXISTS branch_services (
 
 -- Create table for appointments
 CREATE TABLE IF NOT EXISTS appointments (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     branch_service_id INT NOT NULL REFERENCES branch_services(id) ON DELETE CASCADE,
     date DATE NOT NULL,
